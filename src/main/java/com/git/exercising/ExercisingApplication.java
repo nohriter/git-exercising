5bbaab2f17a008b8de15e6a8e1560f2057873798
nori <nohriter@gmail.com>
2023-11-26 18:21:11 +0900

    init: 프로젝트 초기설정

---
+++ src/main/java/com/git/exercising/ExercisingApplication.java
@@ -0,0 +1,13 @@
+package com.git.exercising;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExercisingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExercisingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/git/exercising/ExercisingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.git.exercising;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExercisingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
